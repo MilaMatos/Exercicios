679bd4f4ba486c0a4a8b78c856cf72a2d93e1337
MilaMatos <49792175+MilaMatos@users.noreply.github.com>
2021-10-27 17:57:47 -0300

    Update 1.py

--- Exercícios Python - Estrutura de decisão/1.py
+++ Exercícios Python - Estrutura de decisão/1.py
@@ -1,4 +1,19 @@
-#Faça um Programa que peça dois números e imprima o maior deles.
-a = float(input('Insira um valor: '))
-b = float(input('Insira outro valor: '))
-print('O maior valor é %f' %a) if a > b else print('O maior valor é %f' %b)
+#Faça um programa para a leitura de duas notas parciais de um aluno.
+#O programa deve calcular a média alcançada por aluno e apresentar:
+#A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+#A mensagem "Reprovado", se a média for menor do que sete;
+#A mensagem "Aprovado com Distinção", se a média for igual a dez.
+
+media1 = float(input('Digite a primeira nota: '))
+while media1 < 0 or media1 > 10:
+   media1 = float(input('Erro\nDigite a primeira nota: '))
+media2 = float(input('Digite a segunda nota: '))
+while media2 < 0 or media2 > 10:
+   media2 = float(input('Erro\nDigite a segunda nota: '))
+media = (media1 + media2)/2
+if media < 7:
+   print('Reprovado')
+elif media > 7 and media < 10:
+   print('Aprovado')
+else:
+   print('Aprovado com Distinção')
