87a580ab1c3febd98d6c85fb373b29c13e7b91d6
MilaMatos <49792175+MilaMatos@users.noreply.github.com>
2021-10-27 17:54:05 -0300

    Create 1.py

---
+++ Exercícios Python - Estrutura de decisão/1.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+a = float(input('Insira um valor: '))
+b = float(input('Insira outro valor: '))
+print('O maior valor é %f' %a) if a > b else print('O maior valor é %f' %b)
